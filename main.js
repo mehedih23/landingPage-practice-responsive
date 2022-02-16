2688b53c0952e2868cdccd62b295b1511b3838dc
mehedih23 <nmehedi23@gmail.com>
2022-02-16 10:14:33 +0600

    add-bg-color-with-js

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
+// Section title //
 const sectionTitle = document.getElementsByClassName('section-title');
 for (const sectiontitle of sectionTitle) {
     sectiontitle.style.color = 'gray';
-}
+};
+
+// Players Bakcground //
+const players = document.getElementsByClassName('players');
+for (const player of players) {
+    player.style.backgroundColor = 'rgba(106, 161, 184, 0.23)';
+};
