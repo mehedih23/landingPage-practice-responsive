2af01a812e1beca7cdf54442bbaff8910991da35
mehedih23 <nmehedi23@gmail.com>
2022-02-16 10:46:00 +0600

    add disabled

--- main.js
+++ main.js
@@ -15,6 +15,24 @@ document.getElementById('btn').addEventListener('click', function () {
     const ul = document.getElementById("unordered-list");
     const li = document.createElement('li');
     li.innerText = 'New Item Added';
+    li.classList.add('item-list');
     ul.appendChild(li);
 
+
 });
+
+// Add Number //
+document.getElementById('add-count').addEventListener('click', function () {
+
+    const addCount = document.getElementById('count');
+    let addCountValue = addCount.value;
+
+    addCountValue = parseInt(addCountValue) + 1;
+    addCount.value = addCountValue;
+    console.log(addCount.value);
+
+    if (addCount.value == 5) {
+        document.getElementById('add-count').setAttribute('disabled', '');
+    }
+
+})
