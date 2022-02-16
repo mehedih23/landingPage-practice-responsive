25a79b9046cd506c4f05bc540d73be92a9f2313f
mehedih23 <nmehedi23@gmail.com>
2022-02-16 10:21:09 +0600

    ul-added-by-js

--- main.js
+++ main.js
@@ -9,3 +9,12 @@ const players = document.getElementsByClassName('players');
 for (const player of players) {
     player.style.backgroundColor = 'rgba(106, 161, 184, 0.23)';
 };
+
+// Add Item //
+document.getElementById('btn').addEventListener('click', function () {
+    const ul = document.getElementById("unordered-list");
+    const li = document.createElement('li');
+    li.innerText = 'New Item Added';
+    ul.appendChild(li);
+
+});
